dd35e7ede2393979bf997bc45cfdfe8dcba36b94
Exoezeroes <exoezeroes@gmail.com>
2023-04-13 21:31:40 +0800

    Show user name in home page
    hidden some unneeded column from User

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,6 +5,7 @@ import { provide } from "vue";
 
 const props = defineProps({
   categories: { type: Array },
+  auth: { type: Object },
 });
 
 provide("categories", props.categories);
@@ -26,7 +27,17 @@ export default {
   <main class="pt-8">
     <article class="grid place-items-center">
       <section>
-        <h1 class="text-5xl font-bold">Selamat datang!</h1>
+        <div v-if="auth.user">
+          <h1 class="text-5xl font-bold">
+            Selamat datang,
+            <span
+              v-if="auth.user"
+              class="text-blue-400"
+              v-text="auth.user.name"
+            />!
+          </h1>
+        </div>
+        <h1 v-else class="text-5xl font-bold">Selamat datang!</h1>
         <h2 class="text-center text-lg text-gray-400">Mau cari apa?</h2>
       </section>
 
