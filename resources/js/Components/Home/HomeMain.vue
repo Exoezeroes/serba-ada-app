80e576e803bc69c28fc3c44028906e1bd1a25e3d
Exoezeroes <exoezeroes@gmail.com>
2023-04-15 11:23:16 +0800

    Refactor Home components

---
+++ resources/js/Components/Home/HomeMain.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Categories from "@/Components/Category/Categories.vue";
+
+const props = defineProps({
+  user: { type: Object },
+});
+</script>
+
+<template>
+  <main class="pt-8">
+    <article class="grid place-items-center">
+      <!-- Welcome Message -->
+      <section>
+        <div v-if="user">
+          <h1 class="text-5xl font-bold">
+            Selamat datang,
+            <span
+              class="text-blue-400"
+              v-text="user.name"
+            />!
+          </h1>
+        </div>
+        <h1 v-else class="text-5xl font-bold">Selamat datang!</h1>
+        <h2 class="text-center text-lg text-gray-400">Mau cari apa?</h2>
+      </section>
+
+      <!-- Categories -->
+      <Categories :class="'mt-8 grid w-5/6 grid-cols-4 gap-4'" />
+    </article>
+  </main>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,6 @@
 <script setup>
+import HomeMain from "@/Components/Home/HomeMain.vue";
 import { Head } from "@inertiajs/vue3";
-import Categories from "@/Components/Category/Categories.vue";
 import { provide } from "vue";
 
 const props = defineProps({
@@ -24,24 +24,5 @@ export default {
     <title>Home</title>
   </Head>
 
-  <main class="pt-8">
-    <article class="grid place-items-center">
-      <section>
-        <div v-if="auth.user">
-          <h1 class="text-5xl font-bold">
-            Selamat datang,
-            <span
-              v-if="auth.user"
-              class="text-blue-400"
-              v-text="auth.user.name"
-            />!
-          </h1>
-        </div>
-        <h1 v-else class="text-5xl font-bold">Selamat datang!</h1>
-        <h2 class="text-center text-lg text-gray-400">Mau cari apa?</h2>
-      </section>
-
-      <Categories :class="'mt-8 grid w-5/6 grid-cols-4 gap-4'" />
-    </article>
-  </main>
+  <HomeMain :user="auth.user" />
 </template>
